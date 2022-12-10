360b9ca02d0006aa73de1c623dcf8c0e7118800b
Ramazan Arda Asci <rardaasci@hotmail.com>
2022-12-11 00:03:53 +0300

    Game scene menu controlller added

---
+++ MachineLearningGame/Assets/GameSceneMenuController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameSceneMenuController : MonoBehaviour
+{
+    public GameObject hintScreen;
+    public GameObject tutorialScreen;
+    public GameObject tutorialScreenPlayButton;
+    public GameObject level1Mission1Screen;
+    public GameObject level1HintScreen;
+    public GameObject level1GamePlayScreen;
+    public GameObject level1GamePlayScreenÝnfoPanel;
+    public bool isLevelCompleted;
+    void Start()
+    {
+        isLevelCompleted = true; // kablo baðlantýsý yaptýðým yerde kontrol edip oraya geçiricem
+        IsTutorialLevelCompleted(isLevelCompleted); // üsttekinin aynýsý
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenTutorialScreen()
+    {
+        hintScreen.SetActive(false);
+        tutorialScreen.SetActive(true);
+    }
+
+    public void OpenHintScreen()
+    {
+        hintScreen.SetActive(true);
+        tutorialScreen.SetActive(false);
+    }
+
+    public void IsTutorialLevelCompleted(bool IsCompleted)
+    {
+        if (IsCompleted)
+        {
+            tutorialScreenPlayButton.SetActive(true);
+        }
+        else
+        {
+            tutorialScreenPlayButton.SetActive(false);
+        }
+    }
+
+    public void PlayButton()
+    {
+        tutorialScreen.SetActive(false);
+        level1Mission1Screen.SetActive(true);
+    }
+
+    public void OpenLevel1HintScreen()
+    {
+        level1HintScreen.SetActive(true);
+        level1Mission1Screen.SetActive(false);
+    }
+
+    public void Level1BackToTutorialScreen()
+    {  //level 1mission screendeki back
+        tutorialScreen.SetActive(true);
+        level1Mission1Screen.SetActive(false);
+    }
+
+    public void Level1BackToMissionScreen()
+    { // level1 hint screendeki back
+        level1HintScreen.SetActive(false);
+        level1Mission1Screen.SetActive(true);
+    }
+
+    public void Level1OpenGamePlayScreen()
+    { 
+        level1GamePlayScreen.SetActive(true);
+        level1Mission1Screen.SetActive(false);
+    }
+
+    public void Level1OpenInfoPanel()
+    {
+        level1GamePlayScreenÝnfoPanel.SetActive(true);
+
+    }
+
+    public void Level1CloseInfoPanel()
+    {
+        level1GamePlayScreenÝnfoPanel.SetActive(false);
+
+    }
+
+
+
+
+
+}
